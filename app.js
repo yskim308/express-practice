990c98b90576986ad6b02458d09ddcf50c6d20e4
Young Seo Kim <youngseokim7@gmail.com>
2025-03-29 11:48:34 +0900

    first request

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/:username/messages/:messageid", (req, res) => {
+  console.log(req.params);
+  res.end();
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
